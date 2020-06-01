2bb8653046d2d98218dd20ec548536a1f53dfa46
Jazmin Tolentino <jaz_01_99@hotmail.com>
2020-06-01 16:18:23 -0500

    commit

---
+++ Proyecto/pages/fix/conexion.php
@@ -0,0 +1,17 @@
+<?php
+    //Conexion
+    $servidor = 'localhost';
+    $usuarioBD = 'root';
+    $password = 'root';
+    $basededatos = 'blog';
+    $db = mysqli_connect($servidor, $usuario, $password, $basededatos);
+    
+    //Asegurar que acepte todos los caracteres especiales
+    mysqli_query($db, "SET NAMES 'utf8' ");
+
+    if(mysqli_connect_errno($conexion)){
+		die("Problemas con la conexi&oacute;n al servidor MySQL: ".mysqli_connect_error());
+	}else{
+		mysqli_query($conexion, "SET NAMES 'utf8'"); //Esta instrucción permite guardar eñes y acentos en la BD ;)
+	}
+?>
